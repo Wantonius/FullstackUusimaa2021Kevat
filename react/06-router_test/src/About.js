dcafa52ca8ea0852c560e0a84d5f177b4b2a521c
Erno Hentonen <erno.hentonen@opiframe.com>
2021-04-22 12:54:23 +0300

    router test

--- react/06-router_test/src/About.js
+++ react/06-router_test/src/About.js
@@ -4,7 +4,10 @@ export default class About extends React.Component {
 	
 	render() {
 		return(
+		<div>
 			<h3>This is the about page</h3>
+			<button onClick={() => this.props.history.push("/secret")}>Go to secret page</button>
+		</div>
 		)
 	}
 }
