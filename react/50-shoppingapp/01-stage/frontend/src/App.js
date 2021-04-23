ad196b37d5991eb6eda6b3b2272b0403c2918d2c
Erno Hentonen <erno.hentonen@opiframe.com>
2021-04-23 12:57:06 +0300

    stage 1 frontend

--- react/50-shoppingapp/01-stage/frontend/src/App.js
+++ react/50-shoppingapp/01-stage/frontend/src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import ShoppingForm from './components/ShoppingForm';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+	
+	constructor(props) {
+		super(props);
+		this.state = {
+			list:[]
+		}
+	}
+	
+	componentDidMount() {
+		this.getList();
+	}
+	
+	//REST API
+	
+	getList = () => {
+		let request = {
+			method:"GET",
+			mode:"cors",
+			headers:{"Content-type":"application/json"}
+		}
+		fetch("/api/shopping",request).then((response) => {
+			if(response.ok) {
+				response.json().then((data) => {
+					this.setState({
+						list:data
+					})
+				}).catch((error) => {
+					console.log("Parsing of JSON failed. Reason:",error)
+				})
+			} else {
+				console.log("Server responded with a status:",response.status)
+			}
+		}).catch((error) => {
+			console.log("Connection rejected. Reason:",error);
+		});
+	}
+	
+	addToList = (item) => {
+		let request = {
+			method:"POST",
+			mode:"cors",
+			headers:{"Content-type":"application/json"},
+			body:JSON.stringify(item)
+		}
+		fetch("/api/shopping",request).then((response) => {
+			if(response.ok) {
+				this.getList();
+			} else {
+				console.log("Server responded with a status:",response.status);
+			}
+		}).catch((error) => {
+			console.log("Connection refused. Reason:",error);
+		});
+	}
+	
+	render() {
+	  return (
+		<div className="App">
+			<ShoppingForm addToList={this.addToList}/>
+		</div>
+	  );
+	}
 }
 
 export default App;
