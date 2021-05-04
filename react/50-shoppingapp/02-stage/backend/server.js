ed7ca1a215d086f43cc471274d907531cec2b2a9
Erno Hentonen <erno.hentonen@opiframe.com>
2021-05-04 11:08:05 +0300

    register

--- react/50-shoppingapp/02-stage/backend/server.js
+++ react/50-shoppingapp/02-stage/backend/server.js
@@ -1,10 +1,50 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const apiroutes = require("./routes/apiroutes");
+const bcrypt = require("bcrypt");
 
 let app = express();
 
 app.use(bodyParser.json());
+
+//LOGIN DATABASES
+
+let registeredUsers = [];
+let loggedSessions = [];
+const time_to_live_diff = 3600000;
+
+//MIDDLEWARE
+
+//LOGIN API
+
+app.post("/register",function(req,res) {
+	if(!req.body) {
+		return res.status(400).json({message:"Bad Request 1"});
+	}
+	if(!req.body.password || !req.body.username) {
+		return res.status(400).json({message:"Bad Request 2 "});
+	}
+	if(req.body.username.length < 4 || req.body.password.length < 8) {
+		return res.status(400).json({message:"Bad Request 3 "});
+	}
+	for(let i=0;i<registeredUsers.length;i++) {
+		if(req.body.username === registeredUsers[i].username) {
+			return res.status(409).json({message:"Username is already in use"});
+		}
+	}
+	bcrypt.hash(req.body.password,14,function(err,hash) {
+		if(err) {
+			return res.status(400).json({message:"Bad request 4"});
+		}
+		let user = {
+			username:req.body.username,
+			password:hash
+		}
+		registeredUsers.push(user);
+		console.log(registeredUsers);
+		return res.status(201).json({message:"User registered"});
+	})
+});
 
 app.use("/api",apiroutes);
 
